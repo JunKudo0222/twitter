7013f24b7e91a405995beac2f1967a29236ef2b1
User Azurefennec478 <azurefennec478@www5191.sakura.ne.jp>
2023-07-01 22:39:37 +0900

    ichiranhyouji

---
+++ followeruserslist.php
@@ -0,0 +1,37 @@
+<?php
+ini_set('display_errors','On');
+ini_set('error_reporting',E_ALL & ~E_NOTICE);
+ini_set('error_reporting',E_ALL);
+
+$dbuser="azurefennec478";
+$dbname="azurefennec478_new";
+$dbhost="mysql634.db.sakura.ne.jp";
+$dbpassword="Iojk3231";
+
+
+$dsn = "mysql:dbname=".$dbname.";host=".$dbhost.";charset=utf8";
+
+
+try {
+	$db = new PDO($dsn, $dbuser, $dbpassword);
+} catch (PDOException $e) {
+	echo 'DB接続エラー!: ' . $e->getMessage();
+    echo '<br><a href="index.php">戻る</a>';
+    
+    
+	exit;
+}
+
+
+
+
+$stmt = $db->prepare("SELECT screen_name FROM accounts");
+		$res = $stmt->execute();
+		if( $res ) {
+			$ids = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		}
+foreach($ids as $id){
+
+    echo "https://twitter.com/".$id['screen_name']."/followers<br>";
+}
+exit;
