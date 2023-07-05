9dd3f6feda2d42ffc759f967af90d1fa91852afe
User Azurefennec478 <azurefennec478@www5191.sakura.ne.jp>
2023-07-06 00:23:36 +0900

    change

--- test2.php
+++ test2.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors','On');
-ini_set('error_reporting',E_ALL & ~E_NOTICE);
-ini_set('error_reporting',E_ALL);
-
 $dbuser="azurefennec478";
 $dbname="azurefennec478_new";
 $dbhost="mysql634.db.sakura.ne.jp";
@@ -21,17 +17,13 @@ try {
     
 	exit;
 }
-
-
-
+        
+        
 
 $stmt = $db->prepare("SELECT screen_name FROM accounts");
-		$res = $stmt->execute();
-		if( $res ) {
-			$ids = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		}
-foreach($ids as $id){
-
-    echo $id['screen_name']."<br>";
+$res = $stmt->execute();
+if( $res ) {
+    $ids = $stmt->fetchAll(PDO::FETCH_COLUMN);
 }
-exit;
+
+echo count($ids);
